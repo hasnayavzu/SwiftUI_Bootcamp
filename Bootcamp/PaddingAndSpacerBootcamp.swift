6257d29823a59e527492b3885121e4a50162ef78
hasnayavzu <hasan.yavuz@ozu.edu.tr>
2023-07-08 22:46:57 +0300

    learned about padding

---
+++ Bootcamp/PaddingAndSpacerBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  PaddingAndSpacerBootcamp.swift
+//  Bootcamp
+//
+//  Created by Hasan Yavuz on 8.07.2023.
+//
+
+import SwiftUI
+
+struct PaddingAndSpacerBootcamp: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+        }
+        .padding()
+        .padding(.vertical, 30)
+        .background(
+            Color.white
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0.0, y: 10)
+        )
+        .padding(.horizontal, 10)
+    }
+}
+
+struct PaddingAndSpacerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PaddingAndSpacerBootcamp()
+    }
+}
---
+++ Bootcamp/SpacerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  SpacerBootcamp.swift
+//  Bootcamp
+//
+//  Created by Hasan Yavuz on 8.07.2023.
+//
+
+import SwiftUI
+
+struct SpacerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpacerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SpacerBootcamp()
+    }
+}
