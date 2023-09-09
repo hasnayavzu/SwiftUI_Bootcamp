9b9a518f393119c3e103d8d9481d8df55eb6f12f
hasnayavzu <hasan.yavuz@ozu.edu.tr>
2023-09-09 04:58:06 +0300

    learned about spacers

--- Bootcamp/SpacerBootcamp.swift
+++ Bootcamp/SpacerBootcamp.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct SpacerBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: nil) {
+            Spacer(minLength: 0)
+                .frame(height: 10)
+                .background(Color.orange)
+
+            Rectangle()
+                .frame(width: 50, height: 50)
+
+            Spacer()
+                .frame(height: 10)
+                .background(Color.orange)
+
+            Rectangle()
+                .fill(.red)
+                .frame(width: 50, height: 50)
+
+            Spacer()
+                .frame(height: 10)
+                .background(Color.orange)
+
+            Rectangle()
+                .fill(.green)
+                .frame(width: 50, height: 50)
+
+            Spacer(minLength: 0)
+                .frame(height: 10)
+                .background(Color.orange)
+        }
+        .background(Color.yellow)
+        .padding(.horizontal, 200)
+        .background(Color.blue)
     }
 }
 
